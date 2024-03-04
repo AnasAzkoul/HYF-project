3cb7ec9826874a270bfd32e2671bae8e22b0b977
Anas azkoul <anas@Anass-MacBook-Air.local>
2024-03-04 21:29:10 +0100

    working on testimonials carousel

--- index.js
+++ index.js
@@ -48,21 +48,40 @@ setInterval(() => {
   resetActiveItem();
 }, 5000);
 
-// carousel functionality for recent work ------------------------------------------
+// Testimonials functionality ------------------------------------------
 
-const carousel = document.querySelector(".recent-work-carousel");
-const carouselItem = document.querySelector(".recent-work-carousel-item");
-const btnLeft = document.getElementById("left");
-const btnRight = document.getElementById("right");
+const testimonials = [
+  {
+    title: "Working with Magnolia was a DREAM.",
+    body: "Organic 90’s meditation a enamel pin, pink bespoke trust fund coloring. Book tbh hoodie, salad subway tile vegan a tattoo a garden thrift, tousled. Gluten-free man bun yes plz swag JOMO keytar.",
+    author: "John Doe",
+  },
+  {
+    title: "You won’t regret working with Magnolia.",
+    body: "Edison bulb cronut organic art party dreamcatcher and praxis narwhal shabby chic kale chips cray chillwave cliche and organic 90’s kombucha. Gluten-free man bun yes plz swag JOMO keytar neutral milk hotel.",
+    author: "Mary Doe",
+  },
+  {
+    title: "We’ll treasure our photos for a lifetime.",
+    body: "I’m baby artisan disrupt post-ironic iPhone brunch. Yes plz sriracha pink a thundercats messenger bag. Glossier succulents a authentic poutine. Gluten-free man bun yes plz swag JOMO keytar neutral milk hotel.",
+    author: "Steven Doe",
+  },
+  {
+    title: "The best experience I have ever had.",
+    body: "Organic 90’s meditation a enamel pin, pink bespoke trust fund coloring. Book tbh hoodie, salad subway tile vegan a tattoo a garden thrift, tousled. Gluten-free man bun yes plz swag JOMO keytar.",
+    author: "Angela Smith",
+  },
+];
 
-const carouselItemWidth = carouselItem.clientWidth;
+function getTestimonials(delay) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(testimonials);
+    }, delay);
+  });
+}
 
-console.log(carouselItemWidth); 
-
-btnLeft.addEventListener("click", () => {
-  carousel.scrollLeft -= carouselItemWidth;
+document.addEventListener("DOMContentLoaded", async () => {
+  const testimonials = await getTestimonials();
+  console.log(testimonials);
 });
-
-btnRight.addEventListener("click", () => {
-  carousel.scrollLeft += carouselItemWidth; 
-})
