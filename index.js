db9b3e661aefa6f6f37f58f2393ace8df1d0bf1c
Anas azkoul <anas@Anass-MacBook-Air.local>
2024-03-05 21:50:54 +0100

    finalized the testimonials carousel component

--- index.js
+++ index.js
@@ -50,7 +50,8 @@ setInterval(() => {
 
 // Testimonials functionality ------------------------------------------
 
-const testimonials = [
+// Testimonials data that will be used to mock an an API GET request;
+const testimonialsData = [
   {
     title: "Working with Magnolia was a DREAM.",
     body: "Organic 90’s meditation a enamel pin, pink bespoke trust fund coloring. Book tbh hoodie, salad subway tile vegan a tattoo a garden thrift, tousled. Gluten-free man bun yes plz swag JOMO keytar.",
@@ -73,15 +74,72 @@ const testimonials = [
   },
 ];
 
+const slider = document.querySelector(".slider");
+const leftBtn = document.getElementById("left");
+const rightBtn = document.getElementById("right");
+let index = 1;
+
+// Function that mocks the API GET request
 function getTestimonials(delay) {
   return new Promise((resolve) => {
     setTimeout(() => {
-      resolve(testimonials);
+      resolve(testimonialsData);
     }, delay);
   });
 }
 
+// calling the function on page mount;
 document.addEventListener("DOMContentLoaded", async () => {
-  const testimonials = await getTestimonials();
-  console.log(testimonials);
+  const testimonials = await getTestimonials(1000);
+  appendTestimonialsToParent(testimonials);
+  buildCarousel();
 });
+
+// the testimonial card component
+function createTestimonialsCard(testimonial) {
+  return `
+      <article class="slide">
+        <h3>${testimonial.title}</h3>
+        <p class="slide-body">
+          ${testimonial.body}
+        </p>
+        <p class="slide-author">${testimonial.author}</p>
+    </article>
+  `;
+}
+
+// add each card to the parent with className slider
+function appendTestimonialsToParent(testimonials) {
+  testimonials.forEach((testimonial) => {
+    const testimonialCard = createTestimonialsCard(testimonial);
+    slider.innerHTML += testimonialCard;
+  });
+}
+
+function buildCarousel() {
+  const slides = document.querySelectorAll(".slide");
+  const width = slides[0].clientWidth;
+
+  rightBtn.addEventListener("click", () => {
+    index++;
+    slider.style.transform = "translateX(" + -index * width + "px)";
+    if (index === slides.length - 1) {
+      rightBtn.classList.add("disable");
+    } else {
+      leftBtn.classList.remove("disable");
+    }
+  });
+
+  leftBtn.addEventListener("click", () => {
+    if (index === 0) {
+      leftBtn.classList.add("disable");
+      return 
+    } else {
+      rightBtn.classList.remove("disable");
+    }
+
+    index--;
+    slider.style.transform = "translateX(" + -index * width + "px)";
+
+  });
+}
