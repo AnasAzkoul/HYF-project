07763015b8f64ec0b9544c8e4c27889b286b8293
Anas azkoul <anas@Anass-MacBook-Air.local>
2024-03-08 21:15:02 +0100

    finalized the last touches

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // Navbar background functionality ---------------------------------------
 const header = document.querySelector("header");
+const navLinks = document.querySelectorAll(".desktop-nav-link");
 
 function handleScroll() {
   const scrollTop = window.scrollY;
@@ -14,6 +15,14 @@ function handleScroll() {
 }
 
 document.addEventListener("scroll", handleScroll);
+
+// header.addEventListener("click", (event) => {
+//   console.log(navLinks);
+//   navLinks.forEach(link => link.classList.remove('active-nav-link'));
+
+//   event.target.classList.add('active-nav-link');
+// })
+
 // Hamburger menu functionality ------------------------------------------
 
 const openSideMenuBtn = document.querySelector(".hamburger-btn");
@@ -53,24 +62,24 @@ setInterval(() => {
 // Testimonials data that will be used to mock an an API GET request;
 const testimonialsData = [
   {
-    title: "Working with Magnolia was a DREAM.",
-    body: "Organic 90’s meditation a enamel pin, pink bespoke trust fund coloring. Book tbh hoodie, salad subway tile vegan a tattoo a garden thrift, tousled. Gluten-free man bun yes plz swag JOMO keytar.",
-    author: "John Doe",
+    title: "A DREAM!",
+    body: "Working with Martina was such an easy and fun experience. She has this unique ability to make her clients feel at ease and comfortable. On top of that, her photos just speak for themselves. Thank you! ",
+    author: "Sue Linden",
   },
   {
-    title: "You won’t regret working with Magnolia.",
-    body: "Edison bulb cronut organic art party dreamcatcher and praxis narwhal shabby chic kale chips cray chillwave cliche and organic 90’s kombucha. Gluten-free man bun yes plz swag JOMO keytar neutral milk hotel.",
-    author: "Mary Doe",
+    title: "No regrets.",
+    body: "Wow wow! I can’t express in words how incredible the three day celebration was all thanks to you and your teams hard work! Thank you sooooo much! ",
+    author: "Jake & Marta Smith",
   },
   {
     title: "We’ll treasure our photos for a lifetime.",
-    body: "I’m baby artisan disrupt post-ironic iPhone brunch. Yes plz sriracha pink a thundercats messenger bag. Glossier succulents a authentic poutine. Gluten-free man bun yes plz swag JOMO keytar neutral milk hotel.",
-    author: "Steven Doe",
+    body: "Thank you team for giving me the best day of my life. Not only did I marry the love of my life but I also got to do it in an unforgettable setting & beautiful venue. The pictures are priceless. Thank you for capturing our story. ",
+    author: "Fran & Zak Kennings",
   },
   {
-    title: "The best experience I have ever had.",
-    body: "Organic 90’s meditation a enamel pin, pink bespoke trust fund coloring. Book tbh hoodie, salad subway tile vegan a tattoo a garden thrift, tousled. Gluten-free man bun yes plz swag JOMO keytar.",
-    author: "Angela Smith",
+    title: "The best experience! No Regrets.",
+    body: "My wife and I were fortunate enough to have Martina and her team as our photographers for our wedding. They took great photos of the entire day and made us feel very comfortable. Can’t recommend them enough, one of the best decisions we made for our wedding!",
+    author: "Angela Hemsworth",
   },
 ];
 
@@ -133,13 +142,80 @@ function buildCarousel() {
   leftBtn.addEventListener("click", () => {
     if (index === 0) {
       leftBtn.classList.add("disable");
-      return 
+      return;
     } else {
       rightBtn.classList.remove("disable");
     }
 
     index--;
     slider.style.transform = "translateX(" + -index * width + "px)";
-
   });
 }
+
+// Scroll up functionality ------------------------------------------
+
+const scrollUpBtn = document.querySelector(".scroll-up");
+
+document.addEventListener("scroll", handleScrollBtn);
+
+function handleScrollBtn() {
+  let scrollableHeight =
+    document.documentElement.scrollHeight -
+    document.documentElement.clientHeight;
+  let goldenRatio = 0.3;
+
+  if (document.documentElement.scrollTop / scrollableHeight > goldenRatio) {
+    scrollUpBtn.style.display = "grid";
+  } else {
+    scrollUpBtn.style.display = "none";
+  }
+}
+
+scrollUpBtn.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+});
+
+// Form functionality ------------------------------------------
+
+const form = document.querySelector("form");
+const firstNameInput = document.getElementById("firstName");
+const emailInput = document.getElementById("email");
+const dialog = document.querySelector("dialog");
+const backdrop = document.getElementById("backdrop");
+
+let firstName = "";
+let email = "";
+
+firstNameInput.addEventListener("change", () => {
+  firstName = firstNameInput.value;
+});
+
+emailInput.addEventListener("change", () => {
+  email = emailInput.value;
+});
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  dialog.style.display = "flex";
+  backdrop.style.display = "block";
+  const message = `
+    <span>Hi ${firstName}</span>
+    <span>Thank you so much for trying out the functionality,</span>
+    <span>and for checking out my website</span>
+  `;
+  // append message
+  dialog.innerHTML += message;
+  dialog.classList.add("open-dialog");
+  backdrop.classList.add("open-backdrop");
+  // Clear the input fields;
+  firstNameInput.value = "";
+  emailInput.value = "";
+
+  setTimeout(() => {
+    dialog.style.display = "none";
+    backdrop.style.display = "none";
+  }, 3000);
+});
